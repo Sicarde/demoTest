ac633e727e0dd6a2add415747aa95b97b94e1be7
newin <newin@legion.ch>
2017-05-28 16:28:10 +0200

    more explicit color usage

--- frag.glsl
+++ frag.glsl
@@ -1,5 +1,8 @@
 #version 400 core
 
+#define colour vec3
+#define distColour vec4
+
 in vec3 position;
 out vec3 fragColor;
 varying vec2 v_texcoord;
@@ -15,11 +18,11 @@ mat3 setCamera( in vec3 ro, in vec3 ta, float cr )
     vec3 cv = normalize( cross(cu,cw) );
     return mat3( cu, cv, cw );
 }
-vec2 add(vec2 d1, vec2 d2) {
+distColour add(distColour d1, distColour d2) {
     return (d1.x<d2.x) ? d1 : d2;
 }
 
-float sdSphere( vec3 p, float s ) {
+float sdSphere(vec3 p, float s) {
     return length(p)-s;
 }
 
@@ -31,13 +34,13 @@ float udBox( vec3 p, vec3 b )
     return length(max(abs(p)-b,0.0));
 }
 
-vec2 scene(in vec3 pos) {
-    vec2 r = vec2(sdSphere(pos - vec3(0.0, cos(u_time), 0.0), 0.25), 46.9);
-    r = add(r, vec2(sdSphere(pos - vec3(sin(u_time), 0.25, 0.0), 0.25), 26.9));
-    r = add(r, vec2(udRoundBox(pos - vec3(0.0, -1.0, 0.0), vec3(2., 0.2, 2.2), 0.1), 10.2));
-    r = add(r, vec2(udBox(pos - vec3 (-2.0, 0.0, 0.0), vec3(0.1, 5.0, 4.0)), 50.2));
-    r = add(r, vec2(udBox(pos - vec3 (2.0, 0.0, 0.0), vec3(0.1, 5.0, 4.0)), 90.2));
-    r = add(r, vec2(udBox(pos - vec3 (0.0, 0.0, 2.0), vec3(5.0, 5.0, 0.1)), 75.2));
+distColour scene(in vec3 pos) {
+    distColour r = distColour(sdSphere(pos - vec3(0.0, cos(u_time), 0.0), 0.25), colour(0.9, 0.2, 0.2));
+    r = add(r, distColour(sdSphere(pos - vec3(sin(u_time), 0.25, 0.0), 0.25), colour(0.2, 1.0, 0.4)));
+    r = add(r, distColour(udRoundBox(pos - vec3(0.0, -1.0, 0.0), vec3(2., 0.2, 2.2), 0.1), colour(0.3, 0.7, 0.8)));
+    r = add(r, distColour(udBox(pos - vec3 (-2.0, 0.0, 0.0), vec3(0.1, 5.0, 4.0)), colour(0.1,0.8, 0.3)));
+    r = add(r, distColour(udBox(pos - vec3 (2.0, 0.0, 0.0), vec3(0.1, 5.0, 4.0)), colour(0.4, 0.1, 0.1)));
+    r = add(r, distColour(udBox(pos - vec3 (0.0, 0.0, 2.0), vec3(5.0, 5.0, 0.1)), colour(0.1, 0.0, 0.7)));
     return r;
 }
 
@@ -58,47 +61,46 @@ vec3 Normal( in vec3 pos )
      */
 }
 
-float AO( in vec3 pos, in vec3 nor )
-{
+float AO(in vec3 pos, in vec3 nor) {
     float occ = 0.0;
     float sca = 1.0;
     for( int i=0; i<5; i++ )
     {
 	float hr = 0.01 + 0.12*float(i)/4.0;
 	vec3 aopos =  nor * hr + pos;
-	float dd = scene( aopos ).x;
+	float dd = scene(aopos).x;
 	occ += -(dd-hr)*sca;
 	sca *= 0.95;
     }
-    return clamp( 1.0 - 3.0*occ, 0.0, 1.0 );    
+    return clamp(1.0 - 3.0 * occ, 0.0, 1.0);
 }
 
 #define MAX_RAYMARCH_ITERATIONS 48
 // IQ Raymarching www.iquilezles.org/www/articles/distfunctions/distfunctions.htm
-vec2 castRay(in vec3 rayOrigin, in vec3 rayDirection, out float complexity) {
+distColour castRay(in vec3 rayOrigin, in vec3 rayDirection, out float complexity) {
     const float depthMin = 1.0;
     const float depthMax= 20.0;
 
     float depth = depthMin;
-    float m = -1.0; // what is that ?
+    colour m = colour(0.0, 0.0, 0.0);
     complexity = 0;
     for(int i=0; i < MAX_RAYMARCH_ITERATIONS; i++) {
 	float precis = 0.00005 * depth;
-	vec2 res = scene(rayOrigin + rayDirection * depth);
+	distColour res = scene(rayOrigin + rayDirection * depth);
 	if(res.x < precis || depth > depthMax) {
 	    break;
 	}
 	depth += res.x;
-	m = res.y;
+	m = res.yzw;
 	++complexity;
     }
 
     complexity /= MAX_RAYMARCH_ITERATIONS;
 
     if(depth > depthMax) {
-	m =-1.0;
+	m = colour(0.0, 0.0, 0.0);
     }
-    return vec2(depth, m);
+    return distColour(depth, m);
 }
 
 void main() {
@@ -109,23 +111,19 @@ void main() {
     vec3 dir = setCamera(camera, vec3( -0.5, -0.4, 0.5 ), 0.0) * normalize(vec3(coord, 2.));
 
     float complexity;
-    vec2 data = castRay(camera, dir, complexity);
+    distColour data = castRay(camera, dir, complexity);
 
     vec3 hitPosition = camera + data.x * dir;
     vec3 normal = Normal(hitPosition);
     float AO = AO(hitPosition, normal);
-    vec2 reflection;
+    distColour reflection;
     float refComplexity = 1.0f;
     reflection = castRay(hitPosition, reflect(dir, normal), refComplexity);
 #if 0
     fragColor = vec3(complexity); return;
 #endif
     //data.y = mix(reflection.y, data.y, (sin(u_time) / 2.0) + 0.5); // testing reflections
-    data.y = mix(reflection.y, data.y, 0.6); // testing reflections
-    if(data.y < 1.5) {
-	fragColor = vec3(0.01, 0.01, 0.02);
-    } else {
-	fragColor = 0.45 + 0.35 * sin( vec3(0.05,0.08,0.10) * (data.y - 1.0) );
-	fragColor = mix(fragColor * 0.4, fragColor, AO);
-    }
+    data.yzw = mix(reflection.yzw, data.yzw, 0.6); // testing reflections
+    fragColor = data.yzw;
+    fragColor = mix(fragColor * 0.4, fragColor, AO);
 }
