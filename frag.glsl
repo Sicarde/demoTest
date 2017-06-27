052032ec192f153437822f592b6dfa8f3a67d653
newin <newin@legion.ch>
2017-06-27 22:30:39 +0200

    new shader stuff

--- frag.glsl
+++ frag.glsl
@@ -68,20 +68,44 @@ float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness) {
     return ggx1 * ggx2;
 }
 
+vec3 cookTorrance(vec3 N, vec3 H, vec3 V, vec3 L, float roughness, vec3 F) {
+
+    float NDF = DistributionGGX(N, H, roughness);        
+    float G   = GeometrySmith(N, V, L, roughness);      
+
+    vec3 DFG = NDF * G * F;
+    return DFG / (4 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.001);
+}
+
+vec3 HeTorranceSillionGreenberg(vec3 N, vec3 H, vec3 V, vec3 L, vec3 albedo, float roughness, float metallic, float IOR) {
+    //vec3 F0 = mix(vec3(0.04), albedo, metallic);
+    // IOR examples
+    //water 1.33 
+    //plastic 1.45 
+    //glass 1.5-1.8 
+    //diamond 2.4 
+    //compound materials like wood, stone, concrete etc 3-6 
+    //metals 20-100 
+    float F = fresnel(N, V, IOR);
+    float patchDiameter = 0.5f; // ??
+    float tetha = atan(patchDiameter) * 2;
+    float dw = sin(tetha) * patchDiameter * patchDiameter;
+    float S = GeometrySmith(N, V, L, roughness); // micro shadowing function
+    float G = 1.0f; // ??
+    vec3 delta = vec3(1.0f);
+    return (F * F * exp(-G) * S) / (cos(dot(N, H)) * dw) * delta;
+}
+
 vec3 pbr(vec3 viewDir, vec3 hitPosition, vec3 surfaceNormal, vec3 albedo, vec3 irradiance, float ao, float roughness, float metallic) {
     vec3 N = surfaceNormal;
     vec3 V = viewDir;
 
-    vec3 F0 = vec3(0.04); 
-    F0 = mix(F0, albedo, metallic);
 
+    vec3 F0 = mix(vec3(0.04), albedo, metallic);
     // reflectance equation
     vec3 Lo = vec3(0.0);
 
-
-    //vec3 F    = fresnelSchlick(max(dot(H, V), 0.0), F0);       
     vec3 kD;
-
     for(int i = 0; i < lightSizeArray; ++i) {
 	// calculate per-light radiance
 	vec3 L = normalize(lights[i].pos - hitPosition);
@@ -90,28 +114,21 @@ vec3 pbr(vec3 viewDir, vec3 hitPosition, vec3 surfaceNormal, vec3 albedo, vec3 i
 	float attenuation = 1.0 / (distance * distance);
 	vec3 radiance     = lights[i].color * attenuation;        
 
-	// cook-torrance brdf
-	float NDF = DistributionGGX(N, H, roughness);        
-	float G   = GeometrySmith(N, V, L, roughness);      
-	vec3 F    = fresnelSchlick(max(dot(H, V), 0.0), F0);       
-
-
-	vec3 kS = F;
-	kD = vec3(1.0) - kS;
-	kD *= 1.0 - metallic;
-
-	vec3 nominator    = NDF * G * F;
-	float denominator = 4 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.001; 
-	vec3 brdf = nominator / denominator;
+	vec3 F    = fresnelSchlick(max(dot(H, V), 0.0), F0);
+	vec3 brdf = cookTorrance(N, H, V, L, roughness, F);
+	//vec3 brdf = HeTorranceSillionGreenberg(N, H, V, L, albedo, roughness, metallic, 1.45);
 
 	// add to outgoing radiance Lo
 	float NdotL = max(dot(N, L), 0.0);                
+	vec3 kS = F;
+	kD = vec3(1.0) - kS;
+	kD *= 1.0 - metallic;
 	Lo += (kD * albedo / PI + brdf) * radiance * NdotL; 
     }   
 
-    //vec3 ambient = vec3(0.03) * albedo * ao;
+    vec3 ambient = vec3(0.03) * albedo * ao;
     //vec3 ambient = (kD * diffuse + specular) * ao; 
-    vec3 ambient = (kD * albedo) * ao; 
+    //vec3 ambient = (irradiance * kD * albedo) * ao; 
     vec3 color = ambient + Lo;
 
     color = color / (color + vec3(1.0));
@@ -120,7 +137,6 @@ vec3 pbr(vec3 viewDir, vec3 hitPosition, vec3 surfaceNormal, vec3 albedo, vec3 i
 }
 
 //-------------------------- PBR functions END -------------------
-
 
 mat3 setCamera( in vec3 ro, in vec3 ta, float cr )
 {
@@ -131,8 +147,15 @@ mat3 setCamera( in vec3 ro, in vec3 ta, float cr )
     return mat3( cu, cv, cw );
 }
 distColour add(distColour d1, distColour d2) {
-    return (d1.x<d2.x) ? d1 : d2;
+    return (d1.x < d2.x) ? d1 : d2;
 }
+distColour sub(distColour d1, distColour d2) {
+    return (-d1.x > d2.x) ? d1 : d2;
+}
+distColour inter(distColour d1, distColour d2) {
+    return (d1.x > d2.x) ? d1 : d2;
+}
+
 
 float sdSphere(vec3 p, float s) {
     return length(p)-s;
@@ -147,13 +170,18 @@ float udBox( vec3 p, vec3 b )
 }
 
 distColour scene(in vec3 pos) {
-    distColour r = distColour(sdSphere(pos - vec3(0.0, cos(u_time), 0.0), sin(u_time)), colour(0.9, 0.2, 0.2));
-    r = add(r, distColour(sdSphere(pos - vec3(sin(u_time), 0.25, 0.0), 0.25), colour(0.2, 1.0, 0.4)));
-    r = add(r, distColour(udRoundBox(pos - vec3(0.0, -1.0, 0.0), vec3(2., 0.2, 2.2), 0.1), colour(0.3, 0.7, 0.8)));
-    r = add(r, distColour(udBox(pos - vec3 (-2.0, 0.0, 0.0), vec3(0.1, 5.0, 4.0)), colour(0.1,0.8, 0.3)));
-    r = add(r, distColour(udBox(pos - vec3 (2.0, 0.0, 0.0), vec3(0.1, 5.0, 4.0)), colour(0.4, 0.1, 0.1)));
-    r = add(r, distColour(udBox(pos - vec3 (0.0, 0.0, 2.0), vec3(5.0, 5.0, 0.1)), colour(0.1, 0.0, 0.7)));
-    return r;
+    //distColour r = distColour(sdSphere(pos - vec3(0.0, cos(u_time), 0.0), sin(u_time)), colour(0.9, 0.2, 0.2));
+    //r = add(r, distColour(sdSphere(pos - vec3(sin(u_time), 0.25, 0.0), 0.25), colour(0.2, 1.0, 0.4)));
+    //r = add(r, distColour(udRoundBox(pos - vec3(0.0, -1.0, 0.0), vec3(2., 0.2, 2.2), 0.1), colour(0.3, 0.7, 0.8)));
+    //r = add(r, distColour(udBox(pos - vec3 (-2.0, 0.0, 0.0), vec3(0.1, 5.0, 4.0)), colour(0.1,0.8, 0.3)));
+    //r = add(r, distColour(udBox(pos - vec3 (2.0, 0.0, 0.0), vec3(0.1, 5.0, 4.0)), colour(0.4, 0.1, 0.1)));
+    //r = add(r, distColour(udBox(pos - vec3 (0.0, 0.0, 2.0), vec3(5.0, 5.0, 0.1)), colour(0.1, 0.0, 0.7)));
+
+    distColour r = distColour(udBox(pos - vec3(0.0, -0.9, 0.0), vec3(0.9, 0.1, 9.1)), colour(0.9, 0.9, 0.2));
+    distColour s = distColour(udBox(pos - vec3 (-0.3, sin(u_time), 0.1), vec3(0.2, 0.8, 0.3)), colour(0.1, 0.0, 0.7));
+
+    return sub(r, s);
+    //return add(s,r);
 }
 
 vec3 Normal( in vec3 pos )
@@ -245,7 +273,7 @@ void main() {
     //fragColor = mix(fragColor * 0.4, fragColor, AO);
 
     float roughness = 0.1;
-    float metallic = 0.9;
+    float metallic = 0.1;
     lights[0].pos = vec3(0.9, 0.1, -0.9);
     lights[0].color = vec3(3.0, 3.0, 10.0);
     fragColor = pbr(viewDir, hitPosition, normal, data.yzw, reflection.yzw, AO, roughness, metallic);
--- main.cpp
+++ main.cpp
@@ -1,12 +1,7 @@
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 
-#include <iostream>
-#include <chrono>
 #include "shaders.hh"
 
 #define STRINGIZE2(s) #s
@@ -29,9 +24,8 @@
 #define RESOLUTIONX 1280
 #define RESOLUTIONY 720
 
-#define END 8629
+#define END 10
 
-#ifdef X11_MODE
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <GL/glx.h>
@@ -65,9 +59,6 @@ void initWin() {
     int glx_major, glx_minor;
 
     // FBConfigs were added in GLX version 1.3.
-    if (!glXQueryVersion(display, &glx_major, &glx_minor) || ((glx_major == 4) && (glx_minor < 3)) || (glx_major < 1)) {
-	std::cout << "your openGL isn't cool enough\n";
-    }
 
     int fbcount;
     GLXFBConfig* fbc = glXChooseFBConfig(display, DefaultScreen(display), visual_attribs, &fbcount);
@@ -89,7 +80,7 @@ void initWin() {
 	XFree( vi );
     }
     GLXFBConfig bestFbc = fbc[best_fbc];
-    XFree( fbc );
+    XFree(fbc);
     // Get a visual
     XVisualInfo *vi = glXGetVisualFromFBConfig(display, bestFbc);
     XSetWindowAttributes swa;
@@ -97,7 +88,9 @@ void initWin() {
     swa.background_pixmap = None ;
     swa.border_pixel      = 0;
     swa.event_mask        = StructureNotifyMask;
-    win = XCreateWindow(display, RootWindow(display, vi->screen), 0, 0, RESOLUTIONX, RESOLUTIONY, 0, vi->depth, InputOutput, vi->visual, CWBorderPixel|CWColormap|CWEventMask, &swa);
+    swa.override_redirect = true;
+    //win = XCreateWindow(display, RootWindow(display, vi->screen), 0, 15, RESOLUTIONX, RESOLUTIONY, 0, vi->depth, InputOutput, vi->visual, CWBorderPixel|CWColormap|CWEventMask|CWOverrideRedirect, &swa); // force fixed resolution
+    win = XCreateWindow(display, RootWindow(display, vi->screen), 0, 15, RESOLUTIONX, RESOLUTIONY, 0, vi->depth, InputOutput, vi->visual, CWBorderPixel|CWColormap|CWEventMask, &swa);
     XFree(vi);
     XStoreName(display, win, STRINGIZE(DEMONAME));
     XMapWindow(display, win);
@@ -107,9 +100,7 @@ void initWin() {
     glXCreateContextAttribsARB = (glXCreateContextAttribsARBProc) glXGetProcAddressARB((const GLubyte *) "glXCreateContextAttribsARB");
 
     ctx = 0;
-    static bool ctxErrorOccurred = false;
-    ctxErrorOccurred = false;
-    int context_attribs[] = {
+    static int context_attribs[] = {
 	GLX_CONTEXT_MAJOR_VERSION_ARB, 4,
 	GLX_CONTEXT_MINOR_VERSION_ARB, 3,
 	//GLX_CONTEXT_FLAGS_ARB        , GLX_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB,
@@ -117,13 +108,14 @@ void initWin() {
     };
     ctx = glXCreateContextAttribsARB( display, bestFbc, 0,
 	    True, context_attribs );
-    XSync( display, False );
-    glXMakeCurrent( display, win, ctx );
+    XSync(display, False);
+    glXMakeCurrent(display, win, ctx);
 }
 
-bool shouldContinue(float time) {
+short shouldContinue(float time) {
     if (time < END) {
-	return true;
+	XResizeWindow(display, win, RESOLUTIONX, RESOLUTIONY);
+	return 1;
     } else {
 	glXMakeCurrent(display, 0, 0 );
 	glXDestroyContext(display, ctx);
@@ -131,7 +123,7 @@ bool shouldContinue(float time) {
 	XDestroyWindow( display, win );
 	XFreeColormap( display, cmap );
 	XCloseDisplay( display );
-	return false;
+	return 0;
     }
 }
 
@@ -139,196 +131,37 @@ void swapBuffers() {
     glXSwapBuffers(display, win);
 }
 
-#endif
-
-#ifdef GLFW_MODE
-#include <GLFW/glfw3.h>
-
-GLFWwindow* window = nullptr;
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
-    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-	glfwSetWindowShouldClose(window, GL_TRUE);
-}
-
-void initWin(){
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    window = glfwCreateWindow(RESOLUTIONX, RESOLUTIONY, STRINGIZE(DEMONAME), nullptr, nullptr);
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);
-    glViewport(0, 0, width, height);
-    glfwSetKeyCallback(window, key_callback);
-}
-
-bool shouldContinue(float time) {
-	glfwPollEvents();
-	if (time > END) {
-	    glfwSetWindowShouldClose(window, GLFW_TRUE);
-	}
-	return !glfwWindowShouldClose(window);
-}
-
-void swapBuffers() {
-    glfwSwapBuffers(window);
-}
-
-#elif SDL12_MODE
-#include <SDL/SDL.h>
-
-void initWin() {
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_SetVideoMode(RESOLUTIONX, RESOLUTIONY, 0, SDL_OPENGL);
-    SDL_ShowCursor(SDL_DISABLE);
-}
-
-SDL_Event event;
-bool shouldContinue(float time) {
-    SDL_PollEvent(&event);
-    return (event.key.keysym.sym != SDLK_ESCAPE) || (event.type == SDL_QUIT) || (time > END);
-}
-void swapBuffers() {
-    SDL_GL_SwapBuffers();
-}
-#elif SDL2_MODE
-#include <SDL2/SDL.h>
-SDL_Window* window;
-void initWin() {
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-
-     window = SDL_CreateWindow(STRINGIZE(DEMONAME), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, RESOLUTIONX, RESOLUTIONY, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
-
-     SDL_GLContext context = SDL_GL_CreateContext(window);
-}
-SDL_Event event;
-bool shouldContinue(float time) {
-    SDL_PollEvent(&event);
-    return (event.key.keysym.sym != SDLK_ESCAPE) || (event.type == SDL_QUIT) || (time > END);
-}
-void swapBuffers() {
-    SDL_GL_SwapWindow(window);
-}
-#endif
-
-#define LOGSIZE 1024
 GLuint initGL() {
-     glewExperimental = GL_TRUE;
-     glewInit();
+    glewExperimental = GL_TRUE;
+    glewInit();
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     const GLchar* sptr = vert_glsl;
     glShaderSource(vertexShader, 1, &sptr, &vert_glsl_len);
     glCompileShader(vertexShader);
-    // Check for compile time errors
-    GLint success;
-    GLchar infoLog[LOGSIZE];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-	glGetShaderInfoLog(vertexShader, LOGSIZE, NULL, infoLog);
-	//std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // Fragment shader
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     sptr = frag_glsl;
     glShaderSource(fragmentShader, 1, &sptr, &frag_glsl_len);
     glCompileShader(fragmentShader);
-    // Check for compile time errors
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-	glGetShaderInfoLog(fragmentShader, LOGSIZE, NULL, infoLog);
-	//std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-#if GEOMETRY
-    // Geometry shader
-    GLuint geometryShader = glCreateShader(GL_GEOMETRY_SHADER);
-    sptr = geometry_glsl;
-    glShaderSource(geometryShader, 1, &sptr, &geometry_glsl_len);
-    glCompileShader(geometryShader);
-    // Check for compile time errors
-    glGetShaderiv(geometryShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(geometryShader, LOGSIZE, NULL, infoLog);
-        std::cout << "ERROR::SHADER::GEOMETRY::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-#endif
-
-#if TESSELATION
-    // Tesselation Control shader
-    GLuint tesselationControlShader = glCreateShader(GL_TESS_CONTROL_SHADER);
-    sptr = tesselationCtrl_glsl;
-    glShaderSource(tesselationControlShader, 1, &sptr, &tesselationCtrl_glsl_len);
-    glCompileShader(tesselationControlShader);
-    // Check for compile time errors
-    glGetShaderiv(tesselationControlShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(tesselationControlShader, LOGSIZE, NULL, infoLog);
-        //std::cout << "ERROR::SHADER::TESSELATIONCONTROL::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // Tesselation Evaluation shader
-    GLuint tesselationEvaluationShader = glCreateShader(GL_TESS_EVALUATION_SHADER);
-    sptr = tesselationEval_glsl;
-    glShaderSource(tesselationEvaluationShader, 1, &sptr, &tesselationEval_glsl_len);
-    glCompileShader(tesselationEvaluationShader);
-    // Check for compile time errors
-    glGetShaderiv(tesselationEvaluationShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(tesselationEvaluationShader, LOGSIZE, NULL, infoLog);
-        //std::cout << "ERROR::SHADER::TESSELATIONEVALUATION::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-#endif
 
     // Link shaders
     GLuint shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
-    //glAttachShader(shaderProgram, geometryShader);
-#if TESSELATIONEVALUATION
-    glAttachShader(shaderProgram, tesselationControlShader);
-    glAttachShader(shaderProgram, tesselationEvaluationShader);
-#endif
-    //glProgramParameteriEXT(shaderProgram, GL_GEOMETRY_INPUT_TYPE_EXT, GL_LINES);
-    //glProgramParameteriEXT(shaderProgram, GL_GEOMETRY_OUTPUT_TYPE_EXT, GL_LINE_STRIP);
-    //glProgramParameteriEXT(shaderProgram, GL_GEOMETRY_VERTICES_OUT_EXT, 2);
     glLinkProgram(shaderProgram);
-    // Check for linking errors
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-	glGetProgramInfoLog(shaderProgram, LOGSIZE, NULL, infoLog);
-	//std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
     glUseProgram(shaderProgram);
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
-    //glDeleteShader(geometryShader);
 
-    GLfloat vertices[] = {
+    GLfloat vertices[12] = {
 	-1.0f,  1.0f, 0.0f,  // Top Left
 	1.0f,  1.0f, 0.0f, // Top Right
 	1.0f, -1.0f, 0.0f, // Bottom Right
 	-1.0f, -1.0f, 0.0f// Bottom Left
     };
-    GLuint indices[] = {  // Note that we start from 0!
+    GLuint indices[6] = {  // Note that we start from 0!
 	0, 1, 2,
 	0, 3, 2
     };
-
-    //GLuint indices[] = {  // Note that we start from 0!
-    //    //0, 1, 2,  // First Triangle
-    //    //2, 3, 0   // Second Triangle
-    //    0, 1, 1, 2//, 2, 0, //isoline first triangle
-    //    //2, 3, 3, 0, 0, 2 //isoline second triangle
-    //};
     GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -349,41 +182,38 @@ GLuint initGL() {
     return shaderProgram;
 }
 
+
+//#include <glm/glm.hpp>
+//#include <glm/gtc/matrix_transform.hpp>
+//#include <glm/gtc/type_ptr.hpp>
+//void _start() {
+#include <sys/time.h>
+#include <iostream>
 int main() {
     initWin();
     GLuint shaderProgram = initGL();
-    //glm::mat4 projectionMat = glm::perspective(glm::radians(90.0f), RESOLUTIONX / RESOLUTIONY, 1.0f, 100.0f);
-    //glm::mat4 viewProjMat = projectionMat * glm::lookAt(glm::vec3(0.0f, 6.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
     GLint timeLoc = glGetUniformLocation(shaderProgram, "u_time");
     GLint resLoc = glGetUniformLocation(shaderProgram, "u_resolution");
     //GLint mouseLoc = glGetUniformLocation(shaderProgram, "u_mouse");
-    //GLint mvpLoc = glGetUniformLocation(shaderProgram, "mvp");
-    //GLint rotationLoc = glGetUniformLocation(shaderProgram, "rotation");
-    auto originTime = std::chrono::high_resolution_clock::now();
     float uTime = 0.0f;
+    timeval base;
+    timeval stamp;
+    gettimeofday(&base, 0);
+    //float uTime = 0.0f;
     while(shouldContinue(uTime)) {
+	//asm ("movl $96,%eax\n" "mov %0,%rdi\n" "mov $0,%rsi\n" : "=r" (&stamp)); // gettimeofday syscall
+	gettimeofday(&stamp, 0); // 96
+	uTime = (float((stamp.tv_usec - base.tv_usec) / 1000000.0f)) + float(stamp.tv_sec - base.tv_sec);
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
-	uTime = std::chrono::duration<float, std::milli>(std::chrono::high_resolution_clock::now() - originTime).count();
-	//glm::mat4 mvp = projectionMat * glm::lookAt(glm::vec3(20.0f, 20.0f, 20.0), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f)) * glm::rotate(glm::mat4(1.0f), uTime / 500.0f, glm::vec3(1.0f, 0.0f, 1.0f));
-	//glm::mat4 mvp = projectionMat * glm::lookAt(glm::vec3(3.0f, 3.0f, 3.0), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));// * glm::rotate(glm::mat4(1.0f), uTime / 500.0f, glm::vec3(1.0f, 0.0f, 1.0f));
-	//glm::mat4 rotationGeometry = glm::rotate(glm::mat4(1.0f), 0.0f, glm::vec3(0.0f, 1.0f, 1.0f));
-	//glm::mat4 rotationGeometry = glm::rotate(glm::mat4(1.0f), uTime / 500.0f, glm::vec3(0.0f, 1.0f, 1.0f));
-	//glUniformMatrix4fv(rotationLoc, 1, GL_FALSE, glm::value_ptr(rotationGeometry));
-	//glm::mat4 mvp = projectionMat * glm::lookAt(glm::vec3(0.0f, 0.0f, -3.0), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f)); //from top
-	//glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, glm::value_ptr(mvp));
-	glUniform1f(timeLoc, uTime / 1000.0f);
+
+	glUniform1f(timeLoc, uTime);
 	glUniform2f(resLoc, RESOLUTIONX, RESOLUTIONY);
 
 	// Draw our first triangle
 	//glDrawArrays(GL_POINTS, 0, 4);
-#if TESSELATION
-	glPatchParameteri(GL_PATCH_VERTICES, 2);
-	glDrawElements(GL_PATCHES, 4, GL_UNSIGNED_INT, 0);
-#else
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-#endif
 	swapBuffers();
     }
-    return 0;
+    //asm ("movl $1,%eax\n" "xor %ebx,%ebx\n" "int $128\n");
 }
